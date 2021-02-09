4ca02453f1eb0e906a729dc27144d370c03034f6
atrox3d <atrox3d@gmail.com>
2021-02-09 12:36:26 +0100

    my version

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+menu = Menu()
+coffeemaker = CoffeeMaker()
+moneymachine = MoneyMachine()
+
+choice = "on"
+while choice != "off":
+    drink = None
+    while drink is None:
+        choice = input(f"What would you like? {menu.get_items()}: ").lower()
+        if choice == "off":
+            print("shutting down...")
+            exit()
+        elif choice == "report":
+            coffeemaker.report()
+            moneymachine.report()
+        else:
+            drink = menu.find_drink(choice)
+
+    if coffeemaker.is_resource_sufficient(drink):
+        print(f"That would be {drink.cost}$")
+        if moneymachine.make_payment(drink.cost):
+            coffeemaker.make_coffee(drink)
