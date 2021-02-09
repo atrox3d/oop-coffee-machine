1659219ed80fea9223d693afde2beffaf08bcda6
atrox3d <atrox3d@gmail.com>
2021-02-09 12:44:29 +0100

    official solution

--- main.py
+++ main.py
@@ -6,21 +6,22 @@ menu = Menu()
 coffeemaker = CoffeeMaker()
 moneymachine = MoneyMachine()
 
-choice = "on"
-while choice != "off":
-    drink = None
-    while drink is None:
-        choice = input(f"What would you like? {menu.get_items()}: ").lower()
-        if choice == "off":
-            print("shutting down...")
-            exit()
-        elif choice == "report":
-            coffeemaker.report()
-            moneymachine.report()
-        else:
-            drink = menu.find_drink(choice)
+is_on = True
+while is_on:
+    options = menu.get_items()
 
-    if coffeemaker.is_resource_sufficient(drink):
-        print(f"That would be {drink.cost}$")
-        if moneymachine.make_payment(drink.cost):
-            coffeemaker.make_coffee(drink)
+    drink = None
+    choice = input(f"What would you like? {options}: ").lower()
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffeemaker.report()
+        moneymachine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffeemaker.is_resource_sufficient(drink):
+            print(f"That would be {drink.cost}$")
+            if moneymachine.make_payment(drink.cost):
+                coffeemaker.make_coffee(drink)
+
+print("Shutting down...")
